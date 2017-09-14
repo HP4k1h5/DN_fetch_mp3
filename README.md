f5ae896bbb4ecefc89b4116fd4c1839df49b3761
b0bW <robertwalks@gmail.com>
2017-09-13 22:41:58 -0400

    new file:   README.md added README 	new file:   dn_test.sh added bash file.

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a command line tool to download and play the latest Democracy Now! broadcast as an .mp3 file. 
+
+This is not pulling data from a public api, and may therefore not guaranteed to be stable. 
+
+Please submit issues to the issues page of the repository at https://github.com/HP4k1h5/DN_fetch_mp3.git
+
+©2017 Robert Walker (MIT License)
---
+++ dn_test.sh
@@ -0,0 +1,9 @@
+d=$(date +%Y-%m%d)
+echo $d
+redirect_url=`curl -I http://traffic.libsyn.com/democracynow/dn$d-1.mp3 | perl -n -e '/^Location: (.*)$/ && print "$1\n"'`
+#echo $redirect_url
+clean_url=${redirect_url%$'\r'}
+echo "Support Democracy Now! by donating at: https://www.democracynow.org/donate"
+curl -o dn_show-$d.mp3 $clean_url
+afplay -q 1 dn_show-$d.mp3 
+
