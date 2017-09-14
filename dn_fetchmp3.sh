628179419217d4c3b847e4d00356ddd57f9dd419
b0bW <robertwalks@gmail.com>
2017-09-13 23:29:07 -0400

    minor changes

--- dn_fetchmp3.sh
+++ dn_fetchmp3.sh
@@ -1,9 +1,10 @@
 d=$(date +%Y-%m%d)
 echo $d
-redirect_url=`curl -I http://traffic.libsyn.com/democracynow/dn$d-1.mp3 | perl -n -e '/^Location: (.*)$/ && print "$1\n"'`
+redirect_url=`curl -s -I http://traffic.libsyn.com/democracynow/dn$d-1.mp3 | perl -n -e '/^Location: (.*)$/ && print "$1\n"'`
 #echo $redirect_url
 clean_url=${redirect_url%$'\r'}
+curl -# -o dn_show-$d.mp3 $clean_url
 echo "Support Democracy Now! by donating at: https://www.democracynow.org/donate"
-curl -o dn_show-$d.mp3 $clean_url
+echo "press ctrl-c to quit"
 afplay -q 1 dn_show-$d.mp3 
 #open -a vlc dn_show-$d.mp3
