22ac857f4648cebf3fb5010c40649697c27990a0
b0bW <robertwalks@gmail.com>
2017-09-13 23:54:27 -0400

    added ascii art

--- dn_fetchmp3.sh
+++ dn_fetchmp3.sh
@@ -4,6 +4,7 @@ redirect_url=`curl -s -I http://traffic.libsyn.com/democracynow/dn$d-1.mp3 | per
 #echo $redirect_url
 clean_url=${redirect_url%$'\r'}
 curl -# -o dn_show-$d.mp3 $clean_url
+cat dn_ascii.txt
 echo "Support Democracy Now! by donating at: https://www.democracynow.org/donate"
 echo "press ctrl-c to quit"
 afplay -q 1 dn_show-$d.mp3 
