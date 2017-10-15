545277cd229f746b0b2ebd37e366fa9db92aed4a
b0bW <robertwalks@gmail.com>
2017-10-15 12:34:09 -0400

    modified:   dn_fetchmp3.sh         fixed sunday's issues not downloading

--- dn_fetchmp3.sh
+++ dn_fetchmp3.sh
@@ -4,21 +4,24 @@
 DoW=$(date +%u)
 if [ $DoW -eq 7 ] 
   then
-    echo "It's sunday, DN! does not air on the weekend\n" 
-    read -p "Would you like to download and play Friday's broadcast? [y/n]\n" -n 1 -r
+    echo $"It's sunday, DN! does not air on the weekend." 
+    read -p "Would you like to download and play Friday's broadcast? [y/n]" -n 1 -r
        if [[ $REPLY =~ ^[Nn]$ ]]
         then
-          echo "try again monday at 9:15 am EST!\n exiting..."
+          echo ""
+          echo "try again monday at 9:15 am EST!\\n exiting..."
           exit;
        fi
        if [[ $REPLY =~ ^[Yy]$ ]]
          then 
            d=$(date -j -v-2d +%Y-%m%d)
+       else 
+           d=$(date +%Y-%m%d)
        fi
 fi
 
 #check if the mp3 can be dowloaded, if not try previous day
-d=$(date +%Y-%m%d)
+
 echo $(date +%Y-%m-%d)
 url=http://traffic.libsyn.com/democracynow/dn$d-1.mp3
 status=$(curl -s -w %{http_code} --output /dev/null ${url})
@@ -39,7 +42,7 @@ fi
 #echo "found it!\n"
 redirect_url=`curl -s -I http://traffic.libsyn.com/democracynow/dn$d-1.mp3 | perl -n -e '/^Location: (.*)$/ && print "$1\n"'`
 clean_url=${redirect_url%$'\r'}
-curl -# -o dn_show-$d.mp3 $clean_url
+curl -# -o ~/Documents/CS/DN_fetch_mp3/dn_show-$d.mp3 $clean_url
 cat dn_ascii.txt
 echo "Support Democracy Now! by donating at: https://www.democracynow.org/donate"
 echo "Playing Democracy Now! broadcast from  $d"
